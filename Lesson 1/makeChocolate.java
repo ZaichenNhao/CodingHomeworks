3428cbac2ec77aed7bb6f3d2c47b27180cd6e4b3
ZaichenNhao <55336402+ZaichenNhao@users.noreply.github.com>
2020-06-10 14:04:21 -0700

    I updated to add explanation

--- Lesson 1/makeChocolate.java
+++ Lesson 1/makeChocolate.java
@@ -1,6 +1,19 @@
 import java.util.*;
 import java.lang.Math; 
 
+/*
+We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each).
+Return the number of small bars to use, assuming we always use big bars before small bars. 
+Return -1 if it can't be done.
+
+Example 1: 
+INPUT: (4 , 1, 9)
+OUTPUT: 4
+
+Example 2:
+INPUT: (4, 1, 10)
+OUTPUT: -1
+*/
 class Chocolate {
 
 	public static int makeChocolate(int small, int big, int goal) {
