b70d34263b6425b8de59193d862a9d02f39c8c90
Zaichen <sunny.yuan0333@gmail.com>
2020-06-11 18:14:38 -0700

    added warm up for lesson 2

---
+++ Lesson 2/returnBig.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.lang.Math; 
+
+
+public class Biq {
+
+	returnBiq(int a, int b, int c) {
+
+		//here is where you write your code
+
+	}
+
+	public static void main(String[] args) {
+
+		List<Integer> aTestCases = Arrays.asList(4,4,4,6,4,4,5,9,3,1,1);
+		List<Integer> bTestCases = Arrays.asList(4,4,4,6,4,4,5,9,3,1,1);
+		List<Integer> cTestCases = Arrays.asList(4,4,4,6,4,4,5,9,3,1,1);
+
+		List<Integer> correctTestCases = Arrays.asList(4,4,4,6,4,4,5,9,3,1,1);
+
+		int wrongs = 0;
+
+		for(int i = 0; i<aTestCases.size();i++){
+
+			int current = returnBiq(aTestCases.get(i), bTestCases.get(i), cTestCases.get(i));
+
+			int a = aTestCases.get(i);
+			int b = bTestCases.get(i);
+			int c = cTestCases.get(i);
+			int correct = correctTestCases.get(i);
+ 
+			if(current == correctTestCases.get(i)){
+
+				System.out.println("Test Case (" + a + ", " + b + ", " + c + ") " + ": " + current + " -->" + " ✓");
+
+			}
+
+			else {
+
+				wrongs++;
+
+				System.out.println("Test Case (" + a + ", " + b + ", " + c + ") " + ": " + current + " -->" + " x");
+
+			}
+
+		}
+
+		System.out.println("Total Wrongs : " + wrongs);
+
+	}
+
+}
